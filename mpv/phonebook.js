d1e06cd8043943ffc6321d4f958da8f46c3fe572
fontsie <mrcstvn90@gmail.com>
2014-01-23 22:51:48 +0100

    function checkName and checkNumber, improve readFile

--- mpv/phonebook.js
+++ mpv/phonebook.js
@@ -1,63 +1,58 @@
 function readFile() {
-			var input = document.getElementById('file');
-			if (!input.files.length) {
-				displayAlert('Please select a file!');
+	var input = document.getElementById('file');
+	if (!input.files.length) {
+		displayAlert('Please select a file!');
+		return;
+	}
+
+	var file = input.files[0];
+	input.value = "";
+	var reader = new FileReader();
+	reader.onloadend = function(e) {
+		if (e.target.readyState == FileReader.DONE) {
+			var json = e.target.result;
+			try{
+				phonebook = JSON.parse(json);
+			}catch(e){
+				displayAlert("File is not valid");
 				return;
 			}
-
-			var file = input.files[0];
-			input.value = "";
-			var reader = new FileReader();
-			reader.onloadend = function(e) {
-				if (e.target.readyState == FileReader.DONE) {
-					var json = e.target.result;
-					try{
-						phonebook = JSON.parse(json);
-					}catch(e){
-						displayAlert("File is not valid");
-						return;
-					}
-					var l = phonebook.length;
-					var k = [];
-					for (var i = 0; i < l; i++) {
-						if (phonebook[i].hasOwnProperty('name')){
-							if(!checkName(phonebook[i].name)){
-								k.push(i);
-								continue;
-							}
-						}else{
-							k.push(i);
-							continue;
-						}
-						if (phonebook[i].hasOwnProperty('surname')){
-							if(!checkName(phonebook[i].surname)){
-								k.push(i);
-								continue;
-							}
-						}else{
-							k.push(i);
-							continue;
-						}	
-						if (phonebook[i].hasOwnProperty('number')){
-							if(!checkNumber(phonebook[i].number)){
-								k.push(i);
-								continue;
-							}
-						}else{
-							k.push(i);
-							continue;
-						}
-					}
-					if(k.length>0){
-						for (var i = k.length - 1; i >= 0; i--) {
-							removeRecord(k[i]);
-						}
-						displayAlert("Error: Phonebook loaded, but "+k.length+" record/s have been ignored because they are not valid");
-					}
-					else {
-						displayAlert("Phonebook loaded successfully!");
-					}
+			var l = phonebook.length;
+			var k = [];
+			for (var i = 0; i < l; i++) {
+				if (!phonebook[i].hasOwnProperty('name') || !checkName(phonebook[i].name)){
+					k.push(i);
+					continue;
+				}
+				if (!phonebook[i].hasOwnProperty('surname') || !checkName(phonebook[i].surname) ){
+						k.push(i);
+						continue;
+				}	
+				if (!phonebook[i].hasOwnProperty('number') || !checkNumber(phonebook[i].number)){
+					k.push(i);
+					continue;
 				}
 			}
-			reader.readAsBinaryString(file);
+			if(k.length>0){
+				for (var i = k.length - 1; i >= 0; i--) {
+					removeRecord(k[i]);
+				}
+				displayAlert("Error: Phonebook loaded, but "+k.length+" record/s have been ignored because they are not valid");
+			}
+			else {
+				displayAlert("Phonebook loaded successfully!");
+			}
 		}
+	}
+	reader.readAsBinaryString(file);
+}
+
+function checkName(name){
+	var regexp = /^[\w ]+$/;
+	return regexp.test(name);
+}
+
+function checkNumber(n){
+	var regexp = /^\+\d{1,3} \d+ \d{6,}$/;
+	return regexp.test(n);
+}
